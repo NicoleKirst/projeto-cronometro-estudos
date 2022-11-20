86b6153dd11d3cc101ec5bb859efa9894817ae0c
Nicole Kirst <nicolekirst@outlook.com>
2022-11-20 20:39:28 -0300

    adicionei os eventos

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import Button from '../Button';
 class Form extends React.Component {
+    state = {
+        tarefa: "",
+        tempo: "00:00"
+    }
+
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+        evento.preventDefault()
+    }
     render() {
         return (
-            <form 
+            <form onSubmit={this.adicionarTarefa.bind(this)}
                 className='mt-6 flex flex-col content-center justify-center items-center gap-4  bg-gray-500 w-96 h-72 rounded-lg'>
                 <div>
                     <label 
@@ -14,6 +22,8 @@ class Form extends React.Component {
                         type="text" 
                         name="tarefa"
                         id="tarefa"
+                        value={this.state.tarefa}
+                        onChange={evento => this.setState({...this.state, tarefa: evento.target.value})}
                         placeholder="O que você quer estudar?"
                         required
                         className='w-64 h-12 p-4 rounded-lg bg-gray-600 shadow-inner placeholder-white'
@@ -29,6 +39,8 @@ class Form extends React.Component {
                         step="1"
                         name="tempo"
                         id="tempo"
+                        value={this.state.tempo}
+                        onChange={evento => this.setState({...this.state, tempo: evento.target.value})}
                         min="00:00:00"
                         max="01:30:00" 
                         required
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Item from './Item';
 
 function List() {
-    const tarefas = [{
+    const [tarefas, setTarefas] = useState([{
         tarefa: 'React',
         tempo: '02:00:00'
     }, {
         tarefa: 'Javascript',
         tempo: '01:00:00'
-    }]
+    }]);
     return(
         <aside className='w-96 h-96'>
-            <h2 className='text-white text-xl font-medium mt-4'>Cards de Estudos:</h2>
+            <h2 onClick={() => {
+                setTarefas([...tarefas,{ tarefa: "Estudar", tempo:"00:02:50"}])
+            }}
+                className='text-white text-xl font-medium mt-4'>Cards de Estudos:</h2>
+                
             <ul>
                 {tarefas.map((item, index) => (
                    <Item 
